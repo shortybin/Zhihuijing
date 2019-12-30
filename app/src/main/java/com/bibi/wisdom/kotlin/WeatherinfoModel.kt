23d3fa490712fbd9d04171f4c2a1839da5fe566e
shrotybin <18334786948@163.com>
2019-12-30 20:27:24 +0800

    删除无用代码

--- app/src/main/java/com/bibi/wisdom/kotlin/WeatherinfoModel.kt
+++
@@ -1,9 +0,0 @@
-package com.bibi.wisdom.kotlin
-
-data class WeatherinfoModel constructor(var weatherinfo: WeatherinfoBean){
-
-    data class WeatherinfoBean(
-            val city: String,
-            val cityId: String
-    )
-}
--- app/src/main/java/com/bibi/wisdom/network/ApiService.java
+++ app/src/main/java/com/bibi/wisdom/network/ApiService.java
@@ -1,46 +1,20 @@
 package com.bibi.wisdom.network;
 
 import com.bibi.wisdom.bean.BannerBean;
-import com.bibi.wisdom.bean.BingImageBean;
 import com.bibi.wisdom.bean.DeviceInfoBean;
 import com.bibi.wisdom.bean.DeviceListBean;
 import com.bibi.wisdom.bean.HistoryBean;
 import com.bibi.wisdom.bean.MaintainListBean;
 import com.bibi.wisdom.bean.NoticeBean;
-import com.bibi.wisdom.bean.Subject;
 import com.bibi.wisdom.bean.UserLoginBean;
 import com.bibi.wisdom.bean.base.BaseBean;
-import com.bibi.wisdom.kotlin.WeatherinfoModel;
-
-import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
-
-    /**
-     * Gets top movie.
-     * @return the top movie
-     */
-    @POST("top250")
-    Observable<BaseBean<List<Subject>>> getTopMovies(@Query("start") int start, @Query("count") int count);
-    /**
-     * Gets top movie.
-     * @return the top movie
-     */
-    @GET("http://www.weather.com.cn/adat/sk/{cityId}.html")
-    Observable<BaseBean<WeatherinfoModel>> getWeather(@Path("cityId") String cityId);
-    /**
-     * Gets top movie.
-     * @return the top movie
-     */
-    @GET("https://cn.bing.com/HPImageArchive.aspx?format=js")
-    Observable<BaseBean<List<BingImageBean>>> getBingImage(@Query("n") int number);
 
     @POST("user/loginbypwd")
     Observable<BaseBean<UserLoginBean>> loginByPwd2(@Query("phone") String username, @Query("pwd") String pwd);
