ac21c1f1bb8d7290bc19ce6432adfa42aa27db1e
shrotybin <18334786948@163.com>
2019-12-30 10:21:35 +0800

    阿里云 集成完成

--- app/src/main/java/com/bibi/wisdom/app/BaseApplication.java
+++ app/src/main/java/com/bibi/wisdom/app/BaseApplication.java
@@ -1,13 +1,23 @@
 package com.bibi.wisdom.app;
 
+import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.alibaba.wireless.security.jaq.JAQException;
+import com.alibaba.wireless.security.jaq.SecurityInit;
+import com.aliyun.iot.aep.sdk.apiclient.IoTAPIClientImpl;
+import com.aliyun.iot.aep.sdk.apiclient.emuns.Env;
+import com.aliyun.iot.aep.sdk.log.ALog;
+import com.aliyun.iot.aep.sdk.login.LoginBusiness;
+import com.aliyun.iot.aep.sdk.login.oa.OALoginAdapter;
+import com.aliyun.iot.push.PushManager;
 import com.bibi.wisdom.utils.cache.ShareData;
 import com.uuzuche.lib_zxing.ZApplication;
 import com.vondear.rxtool.RxTool;
 
 public class BaseApplication extends ZApplication {
+    private static final String TAG = "BaseApplication";
     private static BaseApplication app;
 
     @Override
@@ -22,7 +32,9 @@ public class BaseApplication extends ZApplication {
         app = this;
         RxTool.init(this);
         ShareData.init(this);
-//        initImagePicker();
+
+        init(this);
+        PushManager.getInstance().init(this, "114d");
     }
 
 
@@ -31,18 +43,24 @@ public class BaseApplication extends ZApplication {
     }
 
 
-//    private void initImagePicker() {
-//        ImagePicker imagePicker = ImagePicker.getInstance();
-//        imagePicker.setImageLoader(new GlideImageLoader());   //设置图片加载器
-//        imagePicker.setShowCamera(true);  //显示拍照按钮
-//        imagePicker.setCrop(false);        //允许裁剪（单选才有效）
-//        imagePicker.setSaveRectangle(true); //是否按矩形区域保存
-//        imagePicker.setSelectLimit(9);    //选中数量限制
-//        imagePicker.setMultiMode(false);
-//        imagePicker.setStyle(CropImageView.Style.RECTANGLE);  //裁剪框的形状
-////        imagePicker.setFocusWidth(800);   //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
-////        imagePicker.setFocusHeight(800);  //裁剪框的高度。单位像素（圆形自动取宽高最小值）
-////        imagePicker.setOutPutX(1000);//保存文件的宽度。单位像素
-////        imagePicker.setOutPutY(1000);//保存文件的高度。单位像素
-//    }
+    public void init(Application app) {
+
+        // 初始化无线保镖
+        try {
+            SecurityInit.Initialize(app);
+        } catch (JAQException ex) {
+            ALog.e(TAG, "security-sdk-initialize-failed", ex);
+        } catch (Exception ex) {
+            ALog.e(TAG, "security-sdk-initialize-failed", ex);
+        }
+
+        // 初始化 IoTAPIClient
+        IoTAPIClientImpl.InitializeConfig config = new IoTAPIClientImpl.InitializeConfig();
+        config.host = "api.link.aliyun.com";
+        config.apiEnv = Env.RELEASE;
+        config.authCode = "114d";
+
+        IoTAPIClientImpl impl = IoTAPIClientImpl.getInstance();
+        impl.init(app, config);
+    }
 }
