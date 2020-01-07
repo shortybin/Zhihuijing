5d98f590453f6228b60e753e77a4c50b29837a14
shrotybin <18334786948@163.com>
2020-01-07 13:13:40 +0800

    阿里云集成更新

--- app/src/main/java/com/bibi/wisdom/app/BaseApplication.java
+++ app/src/main/java/com/bibi/wisdom/app/BaseApplication.java
@@ -3,11 +3,15 @@ package com.bibi.wisdom.app;
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
+import android.util.Log;
 
 import com.alibaba.wireless.security.jaq.JAQException;
 import com.alibaba.wireless.security.jaq.SecurityInit;
+import com.aliyun.iot.aep.sdk.IoTSmart;
 import com.aliyun.iot.aep.sdk.apiclient.IoTAPIClientImpl;
 import com.aliyun.iot.aep.sdk.apiclient.emuns.Env;
+import com.aliyun.iot.aep.sdk.framework.AApplication;
+import com.aliyun.iot.aep.sdk.framework.config.GlobalConfig;
 import com.aliyun.iot.aep.sdk.log.ALog;
 import com.aliyun.iot.aep.sdk.login.LoginBusiness;
 import com.aliyun.iot.aep.sdk.login.oa.OALoginAdapter;
@@ -16,7 +20,11 @@ import com.bibi.wisdom.utils.cache.ShareData;
 import com.uuzuche.lib_zxing.ZApplication;
 import com.vondear.rxtool.RxTool;
 
-public class BaseApplication extends ZApplication {
+import java.util.List;
+
+import static com.aliyun.iot.aep.sdk.IoTSmart.REGION_CHINA_ONLY;
+
+public class BaseApplication extends AApplication {
     private static final String TAG = "BaseApplication";
     private static BaseApplication app;
 
@@ -34,7 +42,6 @@ public class BaseApplication extends ZApplication {
         ShareData.init(this);
 
         init(this);
-        PushManager.getInstance().init(this, "114d");
     }
 
 
@@ -44,23 +51,40 @@ public class BaseApplication extends ZApplication {
 
 
     public void init(Application app) {
+        ALog.setLevel(ALog.LEVEL_DEBUG);
+        com.aliyun.alink.linksdk.tools.ALog.setLevel(com.aliyun.alink.linksdk.tools.ALog.LEVEL_DEBUG);
+        // 初始化参数配置
+        IoTSmart.InitConfig initConfig = new IoTSmart.InitConfig()
+                // REGION_ALL: 支持连接中国大陆和海外多个接入点，REGION_CHINA_ONLY:直连中国大陆接入点，只在中国大陆出货选这个
+                .setRegionType(REGION_CHINA_ONLY)
+                // 对应控制台上的测试版（PRODUCT_ENV_DEV）和正式版（PRODUCT_ENV_PROD）
+                .setProductEnv(IoTSmart.PRODUCT_ENV_DEV)
+                // 是否打开日志
+                .setDebug(true);
+        GlobalConfig.getInstance().setApiEnv(GlobalConfig.API_ENV_TEST);
+        GlobalConfig.getInstance().setBoneEnv(GlobalConfig.BONE_ENV_TEST);
 
-        // 初始化无线保镖
-        try {
-            SecurityInit.Initialize(app);
-        } catch (JAQException ex) {
-            ALog.e(TAG, "security-sdk-initialize-failed", ex);
-        } catch (Exception ex) {
-            ALog.e(TAG, "security-sdk-initialize-failed", ex);
-        }
+        IoTSmart.getCountryList(new IoTSmart.ICountryListGetCallBack() {
+            @Override
+            public void onSucess(List<IoTSmart.Country> list) {
+                for (int i = 0; i < list.size(); i++) {
+                    Log.d(TAG, "onSucess: " + list.get(i).areaName);
+                    if (list.get(i).areaName.equals("中国大陆")) {
+                        GlobalConfig.getInstance().setCountry(list.get(i), new IoTSmart.ICountrySetCallBack() {
+                            @Override
+                            public void onCountrySet(boolean b) {
+                                // 初始化
+                                IoTSmart.init(app, initConfig);
+                            }
+                        });
+                    }
+                }
+            }
 
-        // 初始化 IoTAPIClient
-        IoTAPIClientImpl.InitializeConfig config = new IoTAPIClientImpl.InitializeConfig();
-        config.host = "api.link.aliyun.com";
-        config.apiEnv = Env.RELEASE;
-        config.authCode = "114d";
+            @Override
+            public void onFail(String s, int i, String s1) {
 
-        IoTAPIClientImpl impl = IoTAPIClientImpl.getInstance();
-        impl.init(app, config);
+            }
+        });
     }
 }
